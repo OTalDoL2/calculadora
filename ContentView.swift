327f6d96305bab553f763bf0fa74c24a402b070b
Lucas M <56313048+OTalDoL2@users.noreply.github.com>
2024-06-08 14:33:05 -0300

    Meu primeiro cód IOS

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWord
+//
+//  Created by Lucas Matheus on 07/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWordApp.swift
+//  HelloWord
+//
+//  Created by Lucas Matheus on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
